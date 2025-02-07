120ed757acf9ba2d94f2f9ff2b04c2a4b25dd8e8
Matt Fannin <mfannin@umich.edu>
2025-02-07 18:29:48 -0500

    Added pandas dataframe to save data to

--- golf_sim.py
+++ golf_sim.py
@@ -1,11 +1,17 @@
 import simpy
 import random
+import pandas as pd 
 from utils import design_golf_course
 from utils import play_round
 from utils import calculate_shot_distance
 
+handicap = "low" ##acceptable values: high, mid, low
+
+##Creating database to store the scores
+column_names_database = ['hole_timestamp', 'hole_yardage', 'hole_par', 'player_score', 'hole_number']
+column_names_to_append = ['hole_timestamp', 'hole_yardage', 'hole_par', 'player_score']
+sim_database = pd.DataFrame(columns=column_names_database)
 sim_data = []
-handicap = "high" ##acceptable values: high, mid, low
 
 ## function for number of holes... to make random yardages...
 ## Then assign values for par
@@ -23,10 +29,14 @@ env = simpy.Environment()
 env.process(play_round(env, shot_range, yardage, par, sim_data)) ## Function is to run the simulation 
 env.run()
 
-print(sim_data)
+sim_to_append = pd.DataFrame(sim_data, columns=column_names_to_append)
+sim_to_append['hole_number'] = sim_to_append.index + 1
 
 
+# Append df_to_append to the original DataFrame df
+sim_database = pd.concat([sim_database, sim_to_append]) #ignore_index=True)
 
 
+print(sim_database)
 
 
