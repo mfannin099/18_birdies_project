8ba760ae42e81afa424d2d64a9b7f5bca607a88f
Matt Fannin <mfannin@umich.edu>
2025-02-16 14:53:22 -0500

    Fast API looks like its working

--- main.py
+++ main.py
@@ -7,66 +7,41 @@ import joblib
 import numpy as np
 
 # Declaring FastAPI instance
-app = FastAPI()
+app = FastAPI(title="Golf Handicap Prediction")
 # Load the trained model
-model = joblib.load('sim_golf_model.pkl')
+model = joblib.load('sim_golf_model_NO_STR.pkl')
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World TESTTTT"}
+    return {"Handicap Predicton": "Returns low, mid, high"}
 
 # Request Body Schema
 class ModelInput(BaseModel):
-    course_name: str
+    # course_name: str
     hole_yardage: float
     hole_par: float
     player_score: float
 
 class ModelPrediction(BaseModel):
-    predicted_class_name: str
+    predicted_handicap: str
 
 @app.post("/predict", response_model=ModelPrediction)
 def predict(data: ModelInput):
     # Convert the input data to a numpy array
     input_data = np.array(
-        [[data.course_name, 
+        [[#data.course_name, 
         data.hole_yardage, 
         data.hole_par, 
         data.player_score]]
     )
 
-    predicted_class_name = model.predict(input_data)
+    predicton = model.predict(input_data)
+    prediction_str = str(predicton[0])
 
-    return ModelPrediction(predicted_class_name=predicted_class_name)
+    return ModelPrediction(predicted_handicap=prediction_str)
 
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
 
-
-
-# @app.post("/") #/predict
-# def predict(data: RequestBody):
-#     # Convert input to DataFrame
-#     test_data = pd.DataFrame([data.dict()])
-
-#     # Make a prediction
-#     prediction = model.predict(test_data)[0]
-
-#     # Return the prediction result
-#     return {"predicted_class": prediction}
-
-
-
-
-# @app.post('/predict')
-# def predict(data: RequestBody):
-#     # Convert input into a DataFrame
-#     test_data = pd.DataFrame([data.dict()])  
-
-#     # Predicting the class
-#     prediction = model.predict(test_data)[0]
-    
-#     # Return the prediction result
-#     return {"predicted_class": prediction}
