b8889d56208c70544549416dde8115bdc0311b8a
mfannin <mattfannin6@gmail.com>
2025-11-11 22:36:02 -0500

    Wrapped up metrics for now and also TODOs

--- score_analysis_app/app.py
+++ score_analysis_app/app.py
@@ -45,3 +45,90 @@ if uploaded_file is not None:
     pl.col("Round Played").dt.month().alias("Month"),
     pl.col("Round Played").dt.year().alias("Year")
 ])
+    
+    st.write("")
+    st.write("")
+
+############################################################ BEGIN Metrics
+
+    col1, col2, col3, col4, col5, col6 = st.columns(6)
+
+    with col1:
+        st.metric(
+            label="Average Score (18 Holes)",
+            value=f"{df.filter(pl.col('18 Holes Played Flg') == 1).select(pl.mean('Total Strokes')).item():.1f}"
+        )
+
+    n=10
+    with col2:
+        avg_last_n = (
+            df_sorted
+            .filter(pl.col('18 Holes Played Flg') == 1)   # only 18-hole rounds
+            .sort('Round Played', descending=True)        # most recent first
+            .head(n)                                     # take top n
+            .select(pl.mean('Total Strokes'))             # compute mean
+            .item()
+        )
+
+        st.metric(
+            label=f"Average Score (18 Holes) - Last {n} Rounds",
+            value=f"{avg_last_n:.1f}"
+        )
+
+    with col3:
+        st.metric(
+            label = "Average Score (9 Holes)",
+            value=f"{df.filter(pl.col('18 Holes Played Flg') == 0).select(pl.mean('Total Strokes')).item():.1f}"
+        )
+
+    with col4:
+        avg_last_n = (
+            df_sorted
+            .filter(pl.col('18 Holes Played Flg') == 0)   # only 9-hole rounds
+            .sort('Round Played', descending=True)        # most recent first
+            .head(n)                                     # take top n
+            .select(pl.mean('Total Strokes'))             # compute mean
+            .item()
+        )
+
+        st.metric(
+            label=f"Average Score (9 Holes) - Last {n} Rounds",
+            value=f"{avg_last_n:.1f}"
+        )
+
+    with col5:
+        st.metric(
+            label="Rounds Logged",
+            value=len(df)
+        )
+    
+    with col6:
+        st.metric(
+            label="Rounds Logged (18 Holes)",
+            value=len(df.filter(pl.col('18 Holes Played Flg') == 1))
+        )
+
+############################################################ END Metrics
+
+    st.write("")
+    st.write("")
+    st.markdown("---")  # simple horizontal line
+
+    #TODO Make Tabs
+
+############################################################ BEGIN Score over time
+    
+
+
+    
+
+    ##TODO: score over time, fit random walk, lin reg, rolling average (10) days
+
+    ##TODO: Distrubution of scores
+
+    ##TODO: Breakdown of Birdie, par, boegey, etc
+
+    ##TODO: Course Breakdowns 
+
+
+    ## Maybe for somethings double all 9 hole scores..?
